fb34d45f800d812e306dfc016b501e9fa94c57c0
kshy <habxp@163.com>
2015-08-25 16:51:01 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-mac-shell
 some shell for my mac
+---
+一些平时MacBooK上用的shell
---
+++ memoryusage.sh
@@ -0,0 +1,16 @@
+ps xmo rss=,pmem=,comm= | while read rss pmem comm; ((n++<5)); do
+ 
+    size="$[rss/1024]";
+    short=$[4-${#size}];
+    size="(${size}M)";
+    i=0;
+    while ((i++ < short)); do size=" $size"; done;
+ 
+    pmem="${pmem%%.*}"
+    if   (($pmem >= 20)); then color=$'\e[31m';
+    elif (($pmem >= 10)); then color=$'\e[33m';
+    else                       color=$'\e[32m ';
+    fi;
+ 
+    echo "$color$pmem% $size $(basename "$comm")"$'\e[0m'"";
+done
